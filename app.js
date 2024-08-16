e6d8ece523793f46ec3f99287aa2554898340287
ozkan <ozkandnz09@gmail.com>
2024-08-16 15:35:56 +0300

    a

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-const body = document.querySelector("body")
-const btn1 = document.querySelector(".btn-click")
-const btn2 = document.querySelector(".btn-over")
-
-btn1.addEventListener("click",()=>{
-
-const r = Math.floor(Math.random() * 255);
-const g = Math.floor(Math.random() * 255);
-const b = Math.floor(Math.random() * 255);
-
-const changedColor = `rgb(${r},${g},${b})`;
-
-document.body.style.backgroundColor = changedColor;
-})
+const body = document.querySelector("body");
+const btn1 = document.querySelector(".btn-click");
+const btn2 = document.querySelector(".btn-over");
+const colorInput = document.querySelector("#colorInput")
+const colorTExt = document.getElementById("colorText")
+const copy = document.getElementById("copy")
 
 
-btn2.addEventListener("mouseover",()=>{
-    const r = Math.floor(Math.random() * 255);
-const g = Math.floor(Math.random() * 255);
-const b = Math.floor(Math.random() * 255);
+btn1.addEventListener("click", () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
 
-const changedColor = `rgb(${r},${g},${b})`;
+  const changedColor = `rgb(${r},${g},${b})`;
 
-document.body.style.backgroundColor = changedColor;
-})
+  document.body.style.backgroundColor = changedColor;
+  colorTExt.textContent = changedColor
+  colorInput.style.color = changedColor
+});
+
+btn2.addEventListener("mouseover", () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  const changedColor = `rgb(${r},${g},${b})`;
+
+  document.body.style.backgroundColor = changedColor;
+  colorTExt.textContent = changedColor
+});
